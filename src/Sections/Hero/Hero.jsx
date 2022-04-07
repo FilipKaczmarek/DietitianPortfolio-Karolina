ad6d4a5b9c70a89e7d64ecd1afa6c3c85e29c0fe
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-07 19:25:34 +0200

    Swiper package installed

---
+++ src/Sections/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+
+export const Hero = ({ className }) => {
+  return (
+    <div>
+        <div className="max-w-screen-xl mx-auto">
+
+        </div>
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Hero;
