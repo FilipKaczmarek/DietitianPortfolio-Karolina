78c7e39f4879af36221716461058fd36ad3c5615
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-07 17:37:03 +0200

    Component Navigation created and added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+
+import Navigation from './Sections/Navigation/Navigation';
+
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      <main>
+
+      </main>
     </div>
   );
 }
