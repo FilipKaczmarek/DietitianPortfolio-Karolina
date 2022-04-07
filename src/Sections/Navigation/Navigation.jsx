88c272bb87ebb7bba65c645d052fb1a223f0f925
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-07 18:23:44 +0200

    WIP styling navigation bar

--- src/Sections/Navigation/Navigation.jsx
+++ src/Sections/Navigation/Navigation.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
 
 import DesktopNavMenu from '../../Components/DesktopNavMenu/DesktopNavMenu.jsx'
 
 export const Navigation = ({ className }) => {
   return (
     <nav className={`border-b-4 border-solid shadow-md ${className ? className : ""}`}>
-      <div className="max-w-screen-xl">
-        <div>
-          <span>Karolina Czajkowska</span>
+      <div className="max-w-screen-xl mx-auto flex flex-row justify-between items-center">
+        <div className="py-5">
+          <span className="italic cursor-pointer">Karolina Czajkowska</span>
+          {/* <Link to='/'></Link> */}
         </div>
         <DesktopNavMenu />
         <div>
