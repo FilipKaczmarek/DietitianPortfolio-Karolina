1011fb481c4917095508c71e333cdafec47a696d
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-09 19:28:17 +0200

    WIP before mapping elements in Swiper

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -9,6 +9,27 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
+const content = [
+  {
+    id: 1,
+    title: "Title 1",
+    textContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+        laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.`,
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    textContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+        laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.`,
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    textContent: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+        laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.`,
+  },
+];
+
 export const Hero = ({ className }) => {
   return (
     <div className="w-screen h-[80vh]">
@@ -18,9 +39,9 @@ export const Hero = ({ className }) => {
         slidesPerView={1}
         navigation={true}
         style={{
-            "--swiper-navigation-color": "#fff",
-            "--swiper-pagination-color": "#fff",
-          }}
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
         pagination={{ clickable: true }}
         speed={600}
         parallax={true}
@@ -40,33 +61,54 @@ export const Hero = ({ className }) => {
           }}
           data-swiper-parallax="-23%"
         ></div>
-        <SwiperSlide>
-          <div className="title" data-swiper-parallax="-300">
-            Slide 2
-          </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
-          <div className="text" data-swiper-parallax="-100">
-            <p>
+        <SwiperSlide className="w-screen h-[80vh]">
+          <div className="max-w-screen-xl mx-auto h-full flex flex-col items-end justify-center">
+            <h1
+              className="title w-6/12 justify-end"
+              data-swiper-parallax="-300"
+            >
+              Title
+            </h1>
+            <p className="text w-6/12" data-swiper-parallax="-100">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
               dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
               laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide className="w-screen h-[80vh]">
-          <div className="max-w-screen-xl mx-auto">Slide 1</div>
+          <div className="max-w-screen-xl mx-auto">
+            <div className="max-w-screen-xl mx-auto">
+              <h1 className="title" data-swiper-parallax="-300">
+                Title
+              </h1>
+              <div className="text" data-swiper-parallax="-100">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam dictum mattis velit, sit amet faucibus felis iaculis
+                  nec. Nulla laoreet justo vitae porttitor porttitor.
+                  Suspendisse in sem justo.
+                </p>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="w-screen h-[80vh]">
-          <div className="max-w-screen-xl mx-auto">Slide 2</div>
-        </SwiperSlide>
-        <SwiperSlide className="w-screen h-[80vh]">
-          <div className="max-w-screen-xl mx-auto">Slide 3</div>
+          <div className="max-w-screen-xl mx-auto">
+            <div className="max-w-screen-xl mx-auto">
+              <h1 className="title" data-swiper-parallax="-300">
+                Title
+              </h1>
+              <div className="text" data-swiper-parallax="-100">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam dictum mattis velit, sit amet faucibus felis iaculis
+                  nec. Nulla laoreet justo vitae porttitor porttitor.
+                  Suspendisse in sem justo.
+                </p>
+              </div>
+            </div>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
