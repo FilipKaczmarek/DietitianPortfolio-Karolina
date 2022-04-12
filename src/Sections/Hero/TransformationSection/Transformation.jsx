774f22812fc4bebc21712e01a8f8098c799de289
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-12 19:39:58 +0200

    Example picture added to Transformation slider

--- src/Sections/Hero/TransformationSection/Transformation.jsx
+++ src/Sections/Hero/TransformationSection/Transformation.jsx
@@ -7,12 +7,12 @@ import { EffectCards } from "swiper";
 import "swiper/css";
 import "swiper/css/effect-cards";
 
-import './transformation.css'
+import "./transformation.css";
 
 export const Transformation = ({ className }) => {
   return (
     <div className="max-w-screen-xl mx-auto flex flex-row items-center my-5 py-5">
-      <div className="w-6/12">
+      <div className="w-6/12 text-center">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta sequi
         asperiores, soluta blanditiis magni natus molestiae eaque modi itaque,
         saepe delectus veritatis sit aspernatur recusandae amet facere sed
@@ -25,15 +25,36 @@ export const Transformation = ({ className }) => {
           modules={[EffectCards]}
           className="transformation-swiper"
         >
-          <SwiperSlide className="transformation-swiper--card">Slide 1</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 2</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 3</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 4</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 5</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 6</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 7</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 8</SwiperSlide>
-          <SwiperSlide className="transformation-swiper--card">Slide 9</SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            <img
+              src="./images/before-after-example.jpeg"
+              alt="Before and After body transformation -  of the first client"
+            />
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 2
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 3
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 4
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 5
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 6
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 7
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 8
+          </SwiperSlide>
+          <SwiperSlide className="transformation-swiper--card">
+            Slide 9
+          </SwiperSlide>
         </Swiper>
       </div>
     </div>
