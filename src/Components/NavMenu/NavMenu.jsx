402f36bb67e4da8fdabc8a4b8437d9f8af70e976
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-13 18:52:02 +0200

    Styling behaviour on breakpoints

--- src/Components/NavMenu/NavMenu.jsx
+++ src/Components/NavMenu/NavMenu.jsx
@@ -7,19 +7,19 @@ import UnorderedList from "../UnorderedList/UnorderedList.jsx";
 export const DesktopNavMenu = ({ className }) => {
   return (
     <div className={`${className ? className : ""}`}>
-      <UnorderedList>
-        {/* <Link to='/'>
+      <UnorderedList className="flex flex-row">
+        <Link to='/' className="md:px-4 lg:px-8">
           <li>Strona główna</li>
         </Link>
-        <Link to='/about'>
+        <Link to='/' className="md:px-4 lg:px-8">
           <li>O mnie</li>
         </Link>
-        <Link>
+        <Link to='/' className="md:px-4 lg:px-8">
           <li>Oferta</li>
         </Link>
-        <Link>
-          <li>Oferta</li>
-        </Link> */}
+        <Link to='/' className="md:px-4 lg:px-8">
+          <li>Kontakt</li>
+        </Link>
       </UnorderedList>
     </div>
   );
--- src/Sections/Navigation/Navigation.jsx
+++ src/Sections/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import NavMenu from "../../Components/NavMenu/NavMenu.jsx";
 import MenuIconOpen from "../../Components/MenuIconOpen/MenuIconOpen.jsx";
@@ -10,16 +10,19 @@ export const Navigation = ({ className }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <nav
-      className={`border-b-4 border-solid shadow-md ${
+      className={`border-b-4 border-solid shadow-md px-8 xl:px-0 ${
         className ? className : ""
       }`}
     >
       <div className="max-w-screen-xl mx-auto flex flex-row justify-between items-center">
-        <div className="py-5">
-          <span className="italic cursor-pointer">Karolina Czajkowska</span>
-          {/* <Link to='/'></Link> */}
+        <div className="py-5 flex flex-row justify-between">
+          <div>
+            <Link to="/">
+              <span className="italic cursor-pointer">Karolina Czajkowska</span>
+            </Link>
+          </div>
         </div>
-        <NavMenu />
+        <NavMenu className="hidden md:block" />
         <button className="py-2.5 cursor-pointer">
           <MenuIconOpen
             className={`w-[40px] h-[40px] ${isMenuOpen ? "hidden" : "block"}`}
