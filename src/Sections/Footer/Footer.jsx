79c4727489959da53c781914faa35e8783f1c169
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-13 18:40:53 +0200

    Footer for desktops created

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { fa8 } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-import './Footer.css'
+import "./Footer.css";
 
 export const Footer = () => {
   return (
@@ -15,16 +15,18 @@ export const Footer = () => {
         alt=""
       />
       <div className="w-screen h-[200px] bg-[#0000005e] absolute inset-0"></div>
-      <div className="footerContentBox w-[70%] h-[200px] z-40 flex justify-evenly items-center">
-        <a href="/">
-          <FontAwesomeIcon icon={fa8} className={"text-8xl"} />
-        </a>
-        <a href="/">
-          <FontAwesomeIcon icon={fa8} className={"text-8xl"} />
-        </a>
-        <a href="/">
-          <FontAwesomeIcon icon={fa8} className={"text-8xl"} />
-        </a>
+      <div className="footerContentBox w-[70%] h-[200px] z-40 flex flex-col justify-center items-center text-white">
+        <div className="flex w-full justify-center items-center my-3">
+          <a href="/" className="px-4">
+            <FontAwesomeIcon icon={faFacebook} className={"text-4xl"} />
+          </a>
+          <a href="/" className="px-4">
+            <FontAwesomeIcon icon={faInstagram} className={"text-4xl"} />
+          </a>
+        </div>
+        <div>
+          <span>Copyright &copy; 2022 by Filip Kaczmarek</span>
+        </div>
       </div>
     </div>
   );
