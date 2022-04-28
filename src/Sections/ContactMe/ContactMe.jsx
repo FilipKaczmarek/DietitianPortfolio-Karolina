25a7780d8796cb16570206f9e13eee64bb81f8a2
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-28 19:09:06 +0200

    basic structure of ContactMe section

--- src/Sections/ContactMe/ContactMe.jsx
+++ src/Sections/ContactMe/ContactMe.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import './ContactMe.css'
+
 export const ContactMe = () => {
   return (
-    <div>
-        
+    <div className="max-w-screen-xl mx-auto h-[calc(100vh_-_268px)] relative flex flex-col justify-center overflow">
+      <div className="">
+        <h1 className="text-5xl">Skontaktuj się ze mną!</h1>
+        <p className="text-base max-w-[50%] my-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi facilis aut ea dolorum, soluta dolorem.</p>
+        {/* form Component */}
+      </div>
+      <div className="bg-black max-w-[500px] max-h-[500px] translate--contactInfo">
+          <h5>Info</h5>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
     </div>
   );
 };
