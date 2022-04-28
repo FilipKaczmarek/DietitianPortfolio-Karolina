3ee113ee7ec8632eeaec48da7b49adafb21952ee
Filip Kaczmarek (Priv) <Feli@macbook-air.home>
2022-04-28 18:33:05 +0200

    ContactMe added as a Route in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navigation from "./Sections/Navigation/Navigation.jsx";
 import Hero from './Sections/Hero/Hero.jsx'
+import ContactMe from './Sections/ContactMe/ContactMe.jsx'
 import Footer from "./Sections/Footer/Footer.jsx";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <main className="font-poppins w-screen">
           <Routes>
             <Route path="/" exact element={<Hero />} />
+            <Route path="/contactMe" exact element={<ContactMe />} />
             {/* <Route path="/" exact component={""} />
             <Route path="/about" component={""} /> */}
           </Routes>
---
+++ src/Sections/ContactMe/ContactMe.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const ContactMe = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+ContactMe.propTypes = {};
+
+export default ContactMe;
